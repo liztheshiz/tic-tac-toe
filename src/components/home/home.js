21db2ba97278df0940e6dc0df69b9c095fbfa9c3
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-02 19:38:35 -0400

    made team uppercase when passing from home to gameview

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,8 +7,8 @@ export default function Home() {
                 <div>TIC.</div>
                 <div>TAC.</div>
                 <div>TOE.</div>
-                <Link to="/game" state={{ team: "x" }}><button className="start-button" data-testid="start-button">X</button></Link>
-                <Link to="/game" state={{ team: "o" }}><button className="start-button" data-testid="start-button">O</button></Link>
+                <Link to="/game" state={{ team: "X" }}><button className="start-button" data-testid="start-button">X</button></Link>
+                <Link to="/game" state={{ team: "O" }}><button className="start-button" data-testid="start-button">O</button></Link>
             </div>
         </div>
     )
