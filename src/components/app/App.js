abf46de52eacee2a7e781376330bff6ae582a202
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-02 09:45:44 -0400

    first attempt implementing routing (not working yet)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,30 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <div className="main-title">
-                <div>TIC.</div>
-                <div>TAC.</div>
-                <div>TOE.</div>
-                <button className="start-button" data-testid="start-button">X</button>
-                <button className="start-button" data-testid="start-button">O</button>
-            </div>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/">
+                    <div className="App">
+                        <div className="main-title">
+                            <div>TIC.</div>
+                            <div>TAC.</div>
+                            <div>TOE.</div>
+                            <button className="start-button" data-testid="start-button">X</button>
+                            <button className="start-button" data-testid="start-button">O</button>
+                        </div>
+                    </div>
+                </Route>
+            </Routes>
+        </Router>
     );
 }
 
