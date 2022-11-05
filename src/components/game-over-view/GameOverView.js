e7d431e103b2799cd1d03b227debd438f8c4a093
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-05 14:47:30 -0400

    initialized working game over screen

--- src/components/game-over-view/GameOverView.js
+++ src/components/game-over-view/GameOverView.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import './GameOverView.css';
 
 export default function GameView() {
-    render(
-        <div>GAME OVER</div>
+    return (
+        <div>
+            <div>GAME OVER</div>
+            <div>Play again?</div>
+            <Link to="/play" state={{ team: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
+            <Link to="/play" state={{ team: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
+        </div>
     )
 }
