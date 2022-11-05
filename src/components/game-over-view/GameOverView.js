1d0a444c8e17152cd6dac1113b2c7970f716143d
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-05 16:07:03 -0400

    passed player instead of team from gameover view as well oops

--- src/components/game-over-view/GameOverView.js
+++ src/components/game-over-view/GameOverView.js
@@ -7,8 +7,8 @@ export default function GameView() {
         <div>
             <div>GAME OVER</div>
             <div>Play again?</div>
-            <Link to="/play" state={{ team: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
-            <Link to="/play" state={{ team: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
+            <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
+            <Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
         </div>
     )
 }
