90b43c49d01ef96a0327a8cadf2c09ddb881d9c5
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-06 12:04:16 -0500

    imported bootstrap css to index (now it works!)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //<React.StrictMode>
