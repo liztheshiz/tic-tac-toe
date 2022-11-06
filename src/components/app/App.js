73206c72dc44fcb64511b13080e80b75e0f40bd9
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-06 12:11:03 -0500

    initial implementation of basic bootstrap

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,10 @@ import {
     Route
 } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 
 import Home from '../home/Home';
@@ -14,11 +18,15 @@ import GameOverView from "../game-over-view/GameOverView";
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/play" element={<GameView />} />
-                <Route path="/gameover" element={<GameOverView />} />
-            </Routes>
+            <Container>
+                <Row className="justify-content-center firstrow">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/play" element={<GameView />} />
+                        <Route path="/gameover" element={<GameOverView />} />
+                    </Routes>
+                </Row>
+            </Container>
         </Router>
     );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './Home.css';
 
 export default function Home() {
     return (
-        <div className="Home">
-            <div className="main-title" data-testid="main-title">
-                <div>TIC.</div>
-                <div>TAC.</div>
-                <div>TOE.</div>
-                <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
-                <Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
-            </div>
-        </div>
+        <Col xs={4} className="Home">
+            <Row className="main-title" data-testid="main-title">
+                <Col>
+                    <Row><Col>TIC.</Col></Row>
+                    <Row><Col>TAC.</Col></Row>
+                    <Row><Col>TOE.</Col></Row>
+                </Col>
+            </Row>
+            <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
+            <Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
+        </Col>
     )
 }
