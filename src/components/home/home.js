2f01cb4c853afc3c1c035078969260fe62e92ef2
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-06 12:18:43 -0500

    improved button format (and removed legacy code for test id's)

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,15 +8,17 @@ import './Home.css';
 export default function Home() {
     return (
         <Col xs={4} className="Home">
-            <Row className="main-title" data-testid="main-title">
+            <Row className="main-title">
                 <Col>
                     <Row><Col>TIC.</Col></Row>
                     <Row><Col>TAC.</Col></Row>
                     <Row><Col>TOE.</Col></Row>
                 </Col>
             </Row>
-            <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
-            <Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
+            <Row className="buttons justify-content-center">
+                <Col xs={2}><Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button">X</button></Link></Col>
+                <Col xs={2}><Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button">O</button></Link></Col>
+            </Row>
         </Col>
     )
 }
