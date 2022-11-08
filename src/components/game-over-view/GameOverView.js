3df5cd7455d0d442741eeae8be325ff5205bde36
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-11-08 13:27:01 -0500

    implemented gameoverview design

--- src/components/game-over-view/GameOverView.js
+++ src/components/game-over-view/GameOverView.js
@@ -1,17 +1,22 @@
 import { Link, useLocation } from 'react-router-dom';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './GameOverView.css';
 
-export default function GameView() {
+export default function GameOverView() {
     const location = useLocation();
     const { winner } = location.state;
 
     return (
-        <div>
-            {winner ? <div>{winner} WINS</div> : <div>GAME OVER</div>}
-            <div>Play again?</div>
-            <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link>
-            <Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link>
-        </div>
+        <Col className="GameOverView">
+            {winner ? <Row className="justify-content-center gameover-message">{winner} WINS</Row> : <Row className="justify-content-center gameover-message">GAME OVER</Row>}
+            <Row className="mt-4 justify-content-center play-again">Play again?</Row>
+            <Row className="mt-5 justify-content-center">
+                <Col xs={1} className="justify-content-center button-col text-center"><Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" data-testid="start-button">X</button></Link></Col>
+                <Col xs={1} className="justify-content-center button-col text-center"><Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button" data-testid="start-button">O</button></Link></Col>
+            </Row>
+        </Col>
     )
 }
