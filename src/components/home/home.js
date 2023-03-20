38d46f175395a7bb619c12c4a338fee419eb1676
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2023-03-20 14:09:11 -0400

    started implementing circle animation DOES NOT WORK, PARTIALLY IMPLEMENTED

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,6 +6,30 @@ import Col from 'react-bootstrap/Col';
 import './Home.css';
 
 export default function Home() {
+    // DOM ELEMENTS
+
+
+    const buttonHover = () => {
+        let rectangleLeftWrapper = document.querySelector(".rectangle-left-wrapper");
+        let rectangleRightWrapper = document.querySelector(".rectangle-right-wrapper");
+        rectangleLeftWrapper.classList.add("red");
+        /*rectangleLeftWrapper.classList.add("rotate");
+        rectangleRightWrapper.classList.add("rotate");
+
+        // Disappears right rectangle (now on left) to "reveal" left circle
+        setTimeout(disappearRight, 1000);*/
+    }
+
+    const buttonUnHover = () => {
+
+    }
+
+    // Makes right rectangle (now on left) disappear
+    function disappearRight() {
+        let rectangleRightWrapper = document.querySelector(".rectangle-right-wrapper");
+        rectangleRightWrapper.classList.add("disappear");
+    }
+
     return (
         <Col xs={5} xl={4} className="Home">
             <Row className="main-title justify-content-center text-left">
@@ -16,7 +40,23 @@ export default function Home() {
                 </Col>
             </Row >
             <Row className="buttons mt-5 justify-content-center">
-                <Col xs={3} className="justify-content-center button-col text-center"><Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button">X</button></Link></Col>
+                <Col xs={3} className="justify-content-center button-col text-center">
+                    <div className="button-container">
+                        <div className="">
+                            <div className="circle">
+                                <div className="circle-left"></div>
+                                <div className="circle-right"></div>
+                            </div>
+                            <div className="rectangle-left-wrapper">
+                                <div className="rectangle-left"></div>
+                            </div>
+                            <div className="rectangle-right-wrapper">
+                                <div className="rectangle-right"></div>
+                            </div>
+                        </div>
+                        <Link to="/play" state={{ player: "X", opponent: "O" }}><button className="start-button" onMouseEnter={buttonHover} onMouseLeave={buttonUnHover}>X</button></Link>
+                    </div>
+                </Col>
                 <Col xs={3} className="justify-content-center button-col text-center"><Link to="/play" state={{ player: "O", opponent: "X" }}><button className="start-button">O</button></Link></Col>
             </Row>
         </Col >
